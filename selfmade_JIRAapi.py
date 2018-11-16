fd41b0e92fa514fa08e7328dfcc1a14755306729
Dmitrii <dimonxxx56@gmail.com>
2018-11-17 02:56:11 +0500

    Add issue parsing

--- selfmade_JIRAapi.py
+++ selfmade_JIRAapi.py
@@ -12,7 +12,8 @@ class Board(object):
         self.issues = []
 
     def findAllIssues(self):
-        self.issues = self.jira.search_issues(jql_str="project="+self.projectKey, maxResults=1000)
+        for issue in self.jira.search_issues(jql_str="project="+self.projectKey, maxResults=1000):
+            self.issues.append(Issue(issue))
 
     def connectToBoard(self):
         options = {'server': self.serverUrl}
@@ -67,11 +68,40 @@ class Board(object):
             return False
         return True
 
+class Issue(object):
+    def __init__(self, issue):
+        self.issue = issue
+        self.raw_value = issue.raw
+        self.id = issue.raw['id']
+        self.link = issue.raw['self']
+        self.key = issue.raw['key']
+        self.typeName = issue.raw['fields']['issuetype']['name']
+        self.typeSubtask = issue.raw['fields']['issuetype']['subtask']
+        self.timespent = issue.raw['fields']['timespent']
+        self.projectId = issue.raw['fields']['project']['id']
+        self.projectName = issue.raw['fields']['project']['name']
+        self.projectKey = issue.raw['fields']['project']['key']
+        self.workRatio = issue.raw['fields']['workratio']
+        self.labels = issue.raw['fields']['labels']
+        self.status = issue.raw['fields']['status']['statusCategory']['name']
+        self.description = issue.raw['fields']['description']
+        self.summary = issue.raw['fields']['summary']
+        self.creatorName = issue.raw['fields']['creator']['name']
+        self.creatorKey = issue.raw['fields']['creator']['key']
+        self.creatorAccountId = issue.raw['fields']['creator']['accountId']
+        self.creatorDisplayName = issue.raw['fields']['creator']['displayName']
+        self.progress = issue.raw['fields']['progress']['progress']
+        self.progressTotal = issue.raw['fields']['progress']['total']
 
 if __name__ == "__main__":
     board = Board('https://megatm.atlassian.net', 'mega.7eam@gmail.com', 'satxon-7tinfu-piTpiz', 'MegaTeam', 'MEGAT board')
     board.connectToBoard()
     print(board.jira.projects())
+
+    #Find all issues example
     board.findAllIssues()
     for issue in board.issues:
-        print(issue.raw)
+        print("Key: " + str(issue.key))
+        print("Summary: " + str(issue.summary))
+        print("Description: " + str(issue.description))
+        print("")
